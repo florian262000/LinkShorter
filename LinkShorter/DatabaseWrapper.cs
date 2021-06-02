215df1b07a24e9240fb615536fe8504294f891b6
florian262000 <34872915+florian262000@users.noreply.github.com>
2021-06-02 21:31:44 +0200

    temp removed config attribute

--- LinkShorter/DatabaseWrapper.cs
+++ LinkShorter/DatabaseWrapper.cs
@@ -5,13 +5,13 @@ namespace LinkShorter
 {
     public class DatabaseWrapper
     {
-        private readonly JObject _config;
+        //private readonly JObject _config;
 
         private static NpgsqlConnection connection;
 
         public DatabaseWrapper(JObject config)
         {
-            this._config = config;
+            //this._config = config;
             var cs =
                 $"Host={config["database"]["host"]};Username={config["database"]["username"]};Password={config["database"]["password"]};Database={config["database"]["name"]}";
 
