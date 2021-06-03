8818e6d19d062cad6b8abac803abd13c7c64dd8d
florian262000 <34872915+florian262000@users.noreply.github.com>
2021-06-03 09:06:43 +0200

    add controller for actual shortPath redirection

---
+++ LinkShorter/Controllers/ShortPathController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkShorter.Controllers
+{
+    [ApiController]
+    [Route("/{shortPath}")]
+    public class ShortPathController
+    {
+        [HttpGet]
+        public string Get(string shortPath)
+        {
+            return shortPath;
+        }
+    }
+}
