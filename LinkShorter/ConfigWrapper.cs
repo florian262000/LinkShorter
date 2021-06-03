b62bdd775ccdaf98880cdc4eb0b2182c4264e572
florian262000 <34872915+florian262000@users.noreply.github.com>
2021-06-03 14:27:40 +0200

    Add ConfigWrapper

---
+++ LinkShorter/ConfigWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace LinkShorter
+{
+    public class ConfigWrapper
+    {
+        private JObject config;
+
+        public ConfigWrapper(JObject config)
+        {
+            this.config = config;
+        }
+
+        public JObject Get()
+        {
+            return this.config;
+        }
+    }
+}
