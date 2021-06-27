7b2daa5fb4b159b7ce5401e480b6db159c4fb3b8
florian262000 <florian_26@outlook.com>
2021-06-27 19:04:14 +0200

    fix for deployVue.sh

--- LinkShorter/deployVue.sh
+++ LinkShorter/deployVue.sh
@@ -1,9 +1,11 @@
-cd frontend
-npm run build
-cd ..
 if [ -d "wwwroot" ]; then
     mkdir wwwroot  
 else 
   rm -r wwwroot/* 
 fi
+
+cd frontend
+npm install 
+npm run build
+cd ..
 cp -r frontend/dist/*  wwwroot/
