89d14bc05355fbc92140d31ba0c9704f80c621c4
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-07-22 00:11:09 +0200

    init frontend v2

--- LinkShorter/frontend/src/App.vue
+++ LinkShorter/frontend/src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <v-app>
-    <v-main>
-      <MainComponent />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
-<script>
-import MainComponent from "./components/MainComponent";
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
+export default Vue.extend({
   name: "App",
-
   components: {
-    MainComponent,
+    HelloWorld,
   },
-
-  data: () => ({
-    //
-  }),
-};
+});
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
