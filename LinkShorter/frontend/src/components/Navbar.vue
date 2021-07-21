d8d20054c3a6511d40f0de3aeb78f713b3ed2d97
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-07-22 01:19:57 +0200

    remove boilerplate; add navbar

---
+++ LinkShorter/frontend/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar app flat dark>
+    <v-spacer></v-spacer>
+    <v-app-bar-title class="text-center text-h3">LinkShorter</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+    <v-btn
+      href="https://github.com/florian262000/LinkShorter"
+      target="_blank"
+      icon
+    >
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
