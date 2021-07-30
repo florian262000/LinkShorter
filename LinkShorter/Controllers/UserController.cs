4428a38f96c933ca040f7ec79bbce78ade146347
florian262000 <florian_26@outlook.com>
2021-07-30 23:14:17 +0200

    add endpoint for account deletion

--- LinkShorter/Controllers/UserController.cs
+++ LinkShorter/Controllers/UserController.cs
@@ -63,7 +63,7 @@ namespace LinkShorter.Controllers
             {
                 // set cookies
                 Response.Cookies.Append("session", _sessionManager.Register(userid));
-                
+
                 Response.Cookies.Append("session", _sessionManager.Register(userid));
                 Console.WriteLine(response.ToString());
 
@@ -74,6 +74,43 @@ namespace LinkShorter.Controllers
                 response["errorMessage"] = "password does not match";
                 return StatusCode(401, response.ToString());
             }
+        }
+
+
+        [HttpDelete]
+        [Route("removeAccount")]
+        /// <response code="200">account removed</response>
+        /// <response code="401">invalid userdata</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public ActionResult RemoveAccount()
+        {
+            var response = new JObject();
+
+            Request.Cookies.TryGetValue("session", out var sessionId);
+            if (!_sessionManager.VerifySession(sessionId))
+            {
+                response["errorMessage"] = "user is not authenticated";
+
+                return StatusCode(401, response);
+            }
+
+            var userId = _sessionManager.GetUserFromSessionId(sessionId);
+
+            // remove all shortlinks
+            var sqlQueryLinks =
+                @$"DELETE FROM links WHERE creatoruuid = '{userId}';";
+            var queryLinks = new NpgsqlCommand(sqlQueryLinks, _databaseWrapper.GetDatabaseConnection());
+            queryLinks.ExecuteNonQuery();
+
+
+            // remove account
+            var sqlQueryAccount =
+                @$"DELETE FROM users WHERE id = '{userId}';";
+            var query = new NpgsqlCommand(sqlQueryAccount, _databaseWrapper.GetDatabaseConnection());
+            query.ExecuteNonQuery();
+
+            return StatusCode(200, response.ToString());
         }
 
         [HttpPost]
@@ -186,7 +223,7 @@ namespace LinkShorter.Controllers
             return StatusCode(200, response.ToString());
         }
 
-        
+
         [Route("getapikey")]
         [HttpPost]
         /// <summary>
