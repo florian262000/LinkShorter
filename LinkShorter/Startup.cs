eaeae7918adcb1588b3ef27942903982c57716c3
florian262000 <florian_26@outlook.com>
2021-08-07 10:48:24 +0200

    remove slash

--- LinkShorter/Startup.cs
+++ LinkShorter/Startup.cs
@@ -64,7 +64,7 @@ namespace LinkShorter
                 {
                     FileProvider = new PhysicalFileProvider(
                         Path.Combine(env.ContentRootPath, "frontend-serve")),
-                    RequestPath = "/"
+                    RequestPath = ""
                 });
             }
 
